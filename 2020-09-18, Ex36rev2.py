4407953975801e92386257fa242caac16c5013a0
vacowboy75 <dngrant@gmail.com>
2020-09-19 16:21:41 -0400

    Added user response options to the start() function

--- 2020-09-18, Ex36rev2.py
+++ 2020-09-18, Ex36rev2.py
@@ -2,18 +2,18 @@
 
 def start():
 	print("""Your airplane was struck by a missle and you have crash-landed on a mountain side.  Your communications were destroyed.  Everyone onboard has perished except for you and your 11 year-old son.  After looking through the rubble, you were able to secure enough food to last three days at the most.  You have no means to alert anyone of your whereabouts.  How will you survive?  Will your son make it, or will you leave him to fend for himself?\n\nAbout 150 yards ahead, you see something unusual.  As you and your son slowly stagger towards the site, you determine that it is a cave.  What will you do?  Will you GO in or STAY outside?""")
-	
+
 	choice = input("Do you want to go in or stay outside?  > ")
-	if choice == "go in":
+	if "GO" in choice or "go in" in choice or "Go In" in choice or "go" in choice:
 		print("The ground tremors and rocks seal you in separating you from your son.  While he can barely hear you, you tell him to hang tight and try to find help.")
 		stepForward()
-	elif choice == "stay outside":
+	elif "STAY" in choice or "stay outside" in choice:
 		death("How are you going to survive if you do nothing?  You lose!")
 	else:
-		death("Don't play yourself!  Your choices are to go in or stay outside.  Bye LOSER!")   
-		
+		death("Don't play yourself!  Your choices are to go in or stay outside.  Bye LOSER!")
+
 def tryAgain():
-	print("Would you like to try again?  Enter 1 for Yes or 2 for No.")	
+	print("Would you like to try again?  Enter 1 for Yes or 2 for No.")
 	while True:
 		try:
 			choice = int(input("> "))
@@ -24,8 +24,8 @@ def tryAgain():
 			break
 		except ValueError:
 			print("Invalid selection!!  Enter 1 for Yes or 2 for No.")
-              	
-	
+
+
 def death(how):
 	print(how)
 	#print("Would you like to try again?  Enter 1 for Yes or 2 for No.")
@@ -40,7 +40,7 @@ def death(how):
 	#		break
 	#	except ValueError:
 	#		print("Invalid selection!!  Enter 1 for Yes or 2 for No.")
-			
+
 def stepForward():
 	print("Slow is smooth, smooth is fast.  Going too fast in an unstable structure will cause it to crumble.  You have no light and you have no idea what is in front of you.  Do you wish to continue?  Enter Y or N.")
 	choice = input("> ")
@@ -59,7 +59,7 @@ def stepForward():
 	else:
 		#print("Upper case letters only please.")
 		death("You must push ahead to save yourself and your son.  You lose!  Try again?")
-		
+
 def findWeapon():
 	print("You can feel the rocks ahead, and one of them seems quite sharp.  Perhaps, if you can grab hold of that rock, you can use it as a tool or weapon.  However, if you grab that rock and could bring certain and instanenous death.  What will you do?  Will you YANK the rock, slowly SHIMMY the rock, or PRAY for miracle?")
 	choice = input("> ")
@@ -72,10 +72,10 @@ def findWeapon():
 		death("I believe in the power of prayer, but your son's survival is dependent upon you doing something for yourself.  You died while praying.")
 	else:
 		death("C'mon Man!")
-		
+
 def batsSnakes():
 	print("You have spent the last hour digging through rubble and you have created a tunnel that leads to a clearing.  While you still can't see, you can feel that no more rocks are immediately ahead of you.  Here, you decide to take a much needed break.\n\nAfter a few minutes, you hear something hissing that sounds like the hiss of a snake.  Your heart rate elevates, but you refuse to panic.  Your prior military training kicks in and you quickly remember that your son's survival is dependant upon you getting through this situation.  You decide to use your rock to poke at that the snake.  However, the sharp edge of the rock was dulled because you used it to dig this point.  What will you do?  Do you THROW the ROCK and hope that you hit the snake, or do you POKE at the SNAKE and hope you have enough of a sharp edge to kill it?")
-	
+
 	choice = input("> ")
 	if choice == "throw rock":
 		death("You threw the rock but landed only a glancing blow on the snake.  The snake lurches at you and bites you.  It administers a lethal dose of venom and kills you within a matter of minutes.  You died.")
@@ -108,14 +108,14 @@ def batsSnakes():
 			break
 		if attempts > 7:
 			death("You have worn yourself poking and missing at the snake.  The snake bites and kills you!")
-	else: 
+	else:
 		#else:
 		#	death("I don't know what that means.")
-			
+
 		print("With the snake dead and you exhausted, you decide to sit down and take another break!  Well deserved.")
-	
+
 def pokeAgain():
-	print("Would you like to try and poke the snake again?  Enter 1 for Yes or 2 for No.")	
+	print("Would you like to try and poke the snake again?  Enter 1 for Yes or 2 for No.")
 	while True:
 		try:
 			choice = int(input("> "))
@@ -126,5 +126,5 @@ def pokeAgain():
 			break
 		except ValueError:
 			print("Invalid selection!!  Enter 1 for Yes or 2 for No.")
-	
+
 start()
